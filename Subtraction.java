7e5e1db4ff3a4d7fb967cc717741744649959e8f
srivani <mulukalasrivani@gmail.com>
2024-12-16 14:39:42 +0530

    subtractionjavaprogram

---
+++ Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction { 
+public static void main(String[] args) {
+ int num1 = 35; 
+int num2 = 10;
+ int difference = num1 - num2;
+ System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+ }
+ }
